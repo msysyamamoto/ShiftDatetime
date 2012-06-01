62df463a92c9547d07045e1944ea1bf838a89314
ymmtmsys <m2yamamoto@gmail.com>
2012-06-02 08:31:50 +0900

    first commit.

---
+++ Date/ShiftDatetime.php
@@ -0,0 +1,74 @@
+<?php
+class ShiftDatetime
+{
+    protected static $offset = 0;
+
+    public static function offset($offset)
+    {
+        self::$offset = intval($offset);
+    }
+
+    public static function create($time = null, DateTimeZone $timezone = null)
+    {
+        if ($time === null) {
+            $interval = new DateInterval('PT' . abs(self::$offset) . 'S');
+            $datetime = new Datetime();
+            if (self::$offset >= 0) {
+                $datetime->add($interval);
+            } else {
+                $datetime->sub($interval);
+            }
+            return $datetime;
+        }
+
+        if ($timezone === null) {
+            return new Datetime($time);
+        }
+
+        return new Datetime($time, $timezone);
+    }
+
+    public static function time()
+    {
+        return time() + self::$offset;
+    }
+
+    public static function date($format, $timestamp = null)
+    {
+        if ($timestamp === null) {
+            $timestamp = slef::time();
+        }
+        return date($format, $timestamp);
+    }
+
+    public static function getdate($timestamp = null)
+    {
+        if ($timestamp === null) {
+            $timestamp = slef::time();
+        }
+        return getdate($format, $timestamp);
+    }
+
+    public static function gettimeofday($return_float = false)
+    {
+        $ds = gettimeofday();
+        $ds['sec'] += self::$offset;
+        return $ds; 
+    }
+
+    public static function gmdate($format, $timestamp = null)
+    {
+        if ($timestamp === null) {
+            $timestamp = slef::time();
+        }
+        return gmdate($format, $timestamp);
+    }
+
+    public static function gmstrftime($format, $timestamp = null)
+    {
+        if ($timestamp === null) {
+            $timestamp = slef::time();
+        }
+        return gmstrftime($format, $timestamp);
+    }
+}
---
+++ tests/ShiftDatetimeTest.php
@@ -0,0 +1,88 @@
+<?php
+require_once realpath(dirname(__FILE__) . '/../Date/ShiftDatetime.php');
+require_once 'PHPUnit/Autoload.php';
+
+class ShiftDatetimeTest extends PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        ShiftDatetime::offset(0);
+    }
+
+    /**
+     * @dataProvider providerTestOffset
+     */ 
+    public function testOffset($offset)
+    {
+        ShiftDatetime::offset($offset);
+        $shiftdate = ShiftDatetime::create(); 
+        $date = new Datetime(); 
+
+        $diff = $shiftdate->getTimestamp() - $date->getTimestamp();
+
+        $this->assertTrue($diff == $offset || $diff == ($offset + 1), "diff=[{$diff}]");
+    } 
+
+    public function providerTestOffset()
+    {
+        return array(
+            array(0),
+            array(59),
+            array(3600),
+            array(3600 * 24),
+            array(3600 * 24 * 365),
+            array(3600 * 24 * 365 * 10),
+            array(-59),
+            array(-3600),
+            array(-3600 * 24),
+            array(-3600 * 24 * 365),
+            array(-3600 * 24 * 365 * 10),
+        );
+    }
+
+    /**
+     * @dataProvider providerTestFormat
+     */ 
+    public function testFormat($format)
+    {
+        ShiftDatetime::offset(123456789);
+        $shiftdate = ShiftDatetime::create($format); 
+        $date = new Datetime($format); 
+
+        $diff = $shiftdate->getTimestamp() - $date->getTimestamp();
+
+        $this->assertEquals(0, $diff);
+    } 
+
+    public function providerTestFormat()
+    {
+        return array(
+            array('2000-01-01'),
+            array('2001-01-01 01:01:01'),
+            array('Apr-17-1990'),
+        );
+    }
+
+    /**
+     * @dataProvider providerTestTimezone
+     */ 
+    public function testTimezone($format, $timezone)
+    {
+        ShiftDatetime::offset(123456789);
+        $shiftdate = ShiftDatetime::create($format, $timezone); 
+        $date = new Datetime($format, $timezone); 
+
+        $diff = $shiftdate->getTimestamp() - $date->getTimestamp();
+
+        $this->assertEquals(0, $diff);
+    } 
+
+    public function providerTestTimezone()
+    {
+        return array(
+            array('2000-01-01', new DateTimeZone('Pacific/Nauru')),
+            array('2001-01-01 01:01:01', new DateTimeZone('Pacific/Nauru')),
+            array('Apr-17-1990', new DateTimeZone('Pacific/Nauru')),
+        );
+    }
+}
