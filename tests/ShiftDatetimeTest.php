44261dc616b6012838a8258676c8f320a637bfc6
ymmtmsys <m2yamamoto@gmail.com>
2012-06-02 09:18:41 +0900

    add new test case.

--- tests/ShiftDatetimeTest.php
+++ tests/ShiftDatetimeTest.php
@@ -11,17 +11,17 @@ class ShiftDatetimeTest extends PHPUnit_Framework_TestCase
 
     /**
      * @dataProvider providerTestOffset
-     */ 
+     */
     public function testOffset($offset)
     {
         ShiftDatetime::offset($offset);
-        $shiftdate = ShiftDatetime::create(); 
-        $date = new Datetime(); 
+        $shiftdate = ShiftDatetime::create();
+        $date = new Datetime();
 
         $diff     = $shiftdate->getTimestamp() - $date->getTimestamp();
         $abs_diff = abs($diff - $offset);
         $this->assertTrue($abs_diff <= 1);
-    } 
+    }
 
     public function providerTestOffset()
     {
@@ -42,17 +42,17 @@ class ShiftDatetimeTest extends PHPUnit_Framework_TestCase
 
     /**
      * @dataProvider providerTestFormat
-     */ 
+     */
     public function testFormat($format)
     {
         ShiftDatetime::offset(123456789);
-        $shiftdate = ShiftDatetime::create($format); 
-        $date = new Datetime($format); 
+        $shiftdate = ShiftDatetime::create($format);
+        $date = new Datetime($format);
 
         $diff = $shiftdate->getTimestamp() - $date->getTimestamp();
 
         $this->assertEquals(0, $diff);
-    } 
+    }
 
     public function providerTestFormat()
     {
@@ -65,17 +65,17 @@ class ShiftDatetimeTest extends PHPUnit_Framework_TestCase
 
     /**
      * @dataProvider providerTestTimezone
-     */ 
+     */
     public function testTimezone($format, $timezone)
     {
         ShiftDatetime::offset(123456789);
-        $shiftdate = ShiftDatetime::create($format, $timezone); 
-        $date = new Datetime($format, $timezone); 
+        $shiftdate = ShiftDatetime::create($format, $timezone);
+        $date = new Datetime($format, $timezone);
 
         $diff = $shiftdate->getTimestamp() - $date->getTimestamp();
 
         $this->assertEquals(0, $diff);
-    } 
+    }
 
     public function providerTestTimezone()
     {
@@ -83,6 +83,37 @@ class ShiftDatetimeTest extends PHPUnit_Framework_TestCase
             array('2000-01-01', new DateTimeZone('Pacific/Nauru')),
             array('2001-01-01 01:01:01', new DateTimeZone('Pacific/Nauru')),
             array('Apr-17-1990', new DateTimeZone('Pacific/Nauru')),
+        );
+    }
+
+    /**
+     * @dataProvider providerTestStatic
+     */
+    public function testDate($offset)
+    {
+        ShiftDatetime::offset($offset);
+
+        $fmt   = 'H,i,s,m,d,Y';
+        $sdate = call_user_func_array(
+            'mktime',
+            explode(',', ShiftDatetime::date($fmt))
+        );
+        $odate = call_user_func_array(
+            'mktime',
+            explode(',', date($fmt))
+        );
+
+        $diff = $sdate - $odate;
+        $test = abs($offset - $diff);
+        $this->assertTrue($test <= 1);
+    }
+
+    public function providerTestStatic()
+    {
+        return array(
+            array(0),
+            array(123456789),
+            array(-987654321),
         );
     }
 }
